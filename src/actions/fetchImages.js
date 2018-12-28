a978fcad44f14e19723ea8e724ffc6a07d224dd8
kakhlaghi <kevin.akhlaghi@gmail.com>
2018-12-27 16:08:50 -0800

    building initial fetch for gallery tags

--- src/actions/fetchImages.js
+++ src/actions/fetchImages.js
@@ -1,13 +1,22 @@
-const url = 'https://imgur.com/gallery'
+const url = 'https://api.imgur.com/3/gallery/t/'
+const api_key = '8292cf41c20143c'
+
+/*const auth = 'Client'*/
 export function fetchImages (dispatch) {
     return function(dispatch){
         dispatch({type: 'LOADING_IMAGES'})
-        return fetch(url)
+        return fetch(url, {
+            headers: {
+              'Authorization': 'Client-ID ' + apiKey
+            }})
         .then(response => {return response.json()})
         .then(responseJSON => {
-            dispatch({type: 'FETCH_IMAGES', payload: responseJSON.images})
+            dispatch({type: 'FETCH_IMAGES', payload: responseJSON.items})
         })
     }
 }
+
+
+
 
 
