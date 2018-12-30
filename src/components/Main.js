110f966448a1063450006d19af9b95cdd3908938
kakhlaghi <kevin.akhlaghi@gmail.com>
2018-12-29 21:26:55 -0800

    adds image list will be container for images

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import ImageListContainer from '../containers/ImageListContainer';
-import UserImageListContainer from '../containers/UserImageListContainer';
 import SignUp from '../containers/SignUp';
 import SignIn from '../containers/SignIn';
 import SignOut from '../containers/SignOut';
@@ -10,7 +8,6 @@ const Main = () => (
   <main>
     <Switch>
       <Route exact path='/' component={Images}/>
-      <Route path='/profile' component={UserImageListContainer} />
     </Switch>
   </main>
 )
---
+++ src/containers/ImageList.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+import Images from '../components/Images'
+
+
+export default class ImageList extends Component {
+
+    renderImages () {
+
+    }
+
+    return(){
+        
+    }
+}
