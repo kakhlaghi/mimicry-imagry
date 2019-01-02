0871bf21841e230ee43cdd59a90be195b9a10ced
kakhlaghi <kevin.akhlaghi@gmail.com>
2019-01-01 20:43:57 -0800

    adds login form to containers

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,20 +1,45 @@
 import React, {Component} from 'react'
 import { format } from 'url';
+/*UI from material-ui*/
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
 
-class Login extends Component {
+
+export default class Login extends Component {
     constructor (props) {
         super(props);
         this.state = {
             name: '',
-            email: '',
             password: ''
         }
     }
 
     render(){
         return (
-            <div>
-
+         <div>
+            <MuiThemeProvider>
+         <div>
+          <AppBar
+             title="Login"
+           />
+           <TextField
+             hintText="Enter your Username"
+             floatingLabelText="Name"
+             onChange = {(event,newValue) => this.setState({name:newValue})}
+             />
+           <br/>
+             <TextField
+               type="password"
+               hintText="Enter your Password"
+               floatingLabelText="Password"
+               onChange = {(event,newValue) => this.setState({password:newValue})}
+               />
+             <br/>
+             <RaisedButton label="Submit" primary={true} style={style} onClick={(event) => this.handleClick(event)}/>
+         </div>
+         </MuiThemeProvider>
             </div>    
         )
     }
