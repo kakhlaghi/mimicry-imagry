35545390a59175e2abb3472618170b4ce97eb618
kakhlaghi <kevin.akhlaghi@gmail.com>
2019-01-01 18:47:39 -0800

    creates basic login class

---
+++ src/containers/Login.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react'
+import { format } from 'url';
+
+class Login extends Component {
+    constructor (props) {
+        super(props);
+        this.state = {
+            name: '',
+            email: '',
+            password: ''
+        }
+    }
+
+    render(){
+        return (
+            <div>
+
+            </div>    
+        )
+    }
+
+}
