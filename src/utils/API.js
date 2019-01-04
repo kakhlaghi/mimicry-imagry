269d30631cb6f8fcb3319a46b64587e0ce026d1a
kakhlaghi <kevin.akhlaghi@gmail.com>
2019-01-03 19:41:02 -0800

    adds api util for fetch and ping api server

---
+++ src/utils/API.js
@@ -0,0 +1,28 @@
+import { api } from './init'
+export function pingApiServer(){
+    return api.get('/')
+        .then(function (response) {
+            return response.data
+        })
+        .catch(function (error) {
+            return error.response.data
+        })
+}
+export function fetchProfilesNoAuth(){
+    return api.get('/profiles')
+        .then(function (response) {
+            return response.data
+        })
+        .catch(function (error) {
+            return error.response.data
+        })
+}
+export function fetchProfilesWithAuth(token){
+    return api.get('/profiles', { headers: { 'Authorization': 'Bearer ' + token } })
+        .then(function (response) {
+            return response.data
+        })
+        .catch(function (error) {
+            return error.response.data
+        })
+}
