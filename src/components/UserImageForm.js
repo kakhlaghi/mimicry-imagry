bb60d3526cb494a297bd966dd24d7c7c30bcd587
kakhlaghi <kevin.akhlaghi@gmail.com>
2019-01-06 13:37:48 -0800

    adds handleInput for form for adding a new user specific image

--- src/components/UserImageForm.js
+++ src/components/UserImageForm.js
@@ -5,8 +5,13 @@ class UserImageFrom extends Component {
     constructor(props){
         super(props),
         this.state={
-
+            imgur_id: this.props.image.imgur_id,
+            user_id: this.props.current_user
         }
+    }
+
+    handleInput=(event)=>{
+        this.setState({[event.target.name]: event.target.value})
     }
 
     render(){
@@ -14,6 +19,7 @@ class UserImageFrom extends Component {
             <div className='tile'>
                 <form>
                     <input className='input' type='text' name='imgur_id' placeholder='add the url for the image here!'/>
+                    <input className='hide' type='number' name='user_id' placeholder={this.state.currentUser.id} />
                 </form>
             </div>
         )
